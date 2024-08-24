d26451e1a1f7fb02f3f424aa84db376ad7102e69
Sulaiman <SulaimanHasan411@gmail.com>
2024-08-24 17:45:01 -0400

    add logic to get user choice, add error if user choice invalid

--- javascript.js
+++ javascript.js
@@ -1,15 +1,29 @@
 let getComputerChoice = parseInt((Math.random() * 3) + 1);
 
-console.log(getComputerChoice)
 
 if (getComputerChoice === 1) {
-    getComputerChoice = "rock"
+    getComputerChoice = "rock";
 }
 else if (getComputerChoice === 2) {
-    getComputerChoice = "paper"
+    getComputerChoice = "paper";
 }
 else if (getComputerChoice === 3) {
-    getComputerChoice = "scissors"
+    getComputerChoice = "scissors";
 }
 
-console.log(getComputerChoice)
+let getUserChoice = prompt("Enter rock, paper, or scissors");
+
+getUserChoice = getUserChoice.toLowerCase();
+
+if (getUserChoice === "rock"){
+    console.log("user choice: rock");
+}
+else if (getUserChoice === "paper"){
+    console.log("user choice: paper");
+}
+else if (getUserChoice === "scissors"){
+    console.log("user choice: scissors");
+}
+else {
+    console.error("not a valid choice");
+}
