06c97b248e67e3e5b47065f9abf987455db57fe8
Sulaiman <SulaimanHasan411@gmail.com>
2024-08-24 18:02:03 -0400

    add code to play a single round, using other functions to see who wins the point given the choices of the user and computer

--- javascript.js
+++ javascript.js
@@ -12,6 +12,8 @@ function getComputerChoice() {
         computerChoice = "scissors";
     }
 
+    console.log(`computer choice: ${computerChoice}`);
+
     return computerChoice;
 }
 
@@ -39,3 +41,33 @@ function getUserChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
+function playRound(humanChoice, machineChoice){
+    if (humanChoice === machineChoice){
+        console.log(`Tie, both chose ${humanChoice}`);
+    }
+    else if (humanChoice === "rock" && machineChoice === "paper"){
+        console.log("You lose, paper beats rock");
+        computerScore++;
+    }
+    else if (humanChoice === "rock" && machineChoice === "scissors"){
+        console.log("You win, rock beats scissors");
+        humanScore++;
+    }
+    else if (humanChoice === "paper" && machineChoice === "rock"){
+        console.log("You win, paper beats rock");
+        humanScore++;
+    }
+    else if (humanChoice === "paper" && machineChoice === "scissors"){
+        console.log("You lose, scissors beats paper");
+        computerScore++;
+    }
+    else if (humanChoice === "scissors" && machineChoice === "rock"){
+        console.log("You lose, rock beats scissors");
+        computerScore++;
+    }
+    else if (humanChoice === "scissors" && machineChoice === "paper"){
+        console.log("You win, scissors beats paper");
+        humanScore++;
+    }
+    console.log(`Your score: ${humanScore}\tComputer score: ${computerScore}`)
+}
